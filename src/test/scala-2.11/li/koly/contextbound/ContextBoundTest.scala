2193e4c40f0a6c2b06b55829abd3847c8c16c2d6
kolyjjj <me.koly.li@gmail.com>
2016-08-24 21:11:31 +0800

    add a test context bound with ordering

--- src/test/scala-2.11/li/koly/contextbound/ContextBoundTest.scala
+++ src/test/scala-2.11/li/koly/contextbound/ContextBoundTest.scala
@@ -8,5 +8,19 @@ class ContextBoundTest extends FunSpec{
       assert(ContextBound.run(new Apple("Hongfushi")) == "Hongfushi grows 1 years.")
       assert(ContextBound.run(new Apple("baitong")) == "baitong grows 1 years.")
     }
+
+    it("a implicitly example with ordering") {
+      implicit val test = new Ordering[Int] {
+        override def compare(x: Int, y: Int): Int = (x - y) match {
+          case z if z > 0 => -1
+          case z if z == 0 => 0
+          case _ => 1
+        }
+      }
+
+      def myCompare[T : Ordering](a: T, b: T): T = if (implicitly[Ordering[T]].lt(a, b)) a else b
+
+      assert(myCompare(4, 5) == 5)
+    }
   }
 }
