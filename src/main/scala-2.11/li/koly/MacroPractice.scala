740f657cfd7937624e04b2acb711d456a93f2ec0
kolyjjj <me.koly.li@gmail.com>
2016-08-24 12:35:29 +0800

    write a simplest test for placeholder

---
+++ src/main/scala-2.11/li/koly/MacroPractice.scala
@@ -0,0 +1,5 @@
+package li.koly
+
+object MacroPractice {
+
+}
---
+++ src/test/scala-2.11/li/koly/MacroPracticeTest.scala
@@ -0,0 +1,11 @@
+package li.koly
+
+import org.scalatest.FunSpec
+
+class MacroPracticeTest extends FunSpec{
+  describe("Macro") {
+    it("should pass") {
+      assert(true)
+    }
+  }
+}
